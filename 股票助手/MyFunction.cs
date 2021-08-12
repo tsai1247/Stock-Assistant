d773d5025c3c17db559e4e2d6b1efae10acb130f
tsai1247 <jeff29cc91079@gmail.com>
2021-08-12 19:49:16 +0800

    [Add] fileRW functions as a class

---
+++ 股票助手/MyFunction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace 股票助手
+{
+    class MyFunction
+    {
+    }
+
+    public class FileRW
+    {
+        public static string[] ReadLineFile(String FilePath)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(FilePath, true);
+                string[] ans = reader.ReadToEnd().Split("\r\n");
+                reader.Close();
+                return ans;
+            }
+            catch (Exception)
+            {
+                return new string[] { };
+            }
+
+        }
+
+        public static void AppendLineFile(String FilePath, String Data)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, true);
+            writer.WriteLine(Data);
+            writer.Close();
+        }
+        public static void AppendLineFile(String FilePath, String[] SpiltData)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, true);
+            foreach (String str in SpiltData)
+                writer.WriteLine(str);
+            writer.Close();
+        }
+
+        public static string Concat(string[] data, string concatSymbol = "\r\n")
+        {
+            if (data.Length == 0)
+                return "";
+            string ret = data[0];
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i] == "") { }
+                else if (data[i] == concatSymbol)
+                    ret += concatSymbol;
+                else
+                    ret += concatSymbol + data[i];
+            }
+            return ret;
+        }
+
+        public static void WriteLineFile(String FilePath, String Data)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, false);
+            writer.WriteLine(Data);
+            writer.Close();
+        }
+
+        public static void WriteLineFile(String FilePath, String[] SpiltData)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, false);
+            foreach (String str in SpiltData)
+                writer.WriteLine(str);
+            writer.Close();
+        }
+
+
+    }
+}
